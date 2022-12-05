fadc89fb8529fea9135ec716db0474ae0e7d076c
Teodora Farcas <tfarcas@confluent.io>
2022-12-05 12:41:01 +0200

    Delete version property from terraform azure helm cfk.

--- terraform/azure/helm_cfk.tf
+++ terraform/azure/helm_cfk.tf
@@ -8,6 +8,5 @@ resource "helm_release" "helm_cfk" {
   name       = "${var.prefix}-${var.project}-cfk-release"
   repository = "https://packages.confluent.io/helm"
   chart      = "confluent-for-kubernetes"
-  version    = "2.5.0"
   namespace  = kubernetes_namespace.aks_ns[0].id
 }
